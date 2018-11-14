0d78acc76eed5675764afa6ef26158fbef7d1a85
jgmze <jgmze@DESKTOP-FBQLBGD>
2018-11-14 13:02:04 -0500

    cambio en rt = 4

---
+++ pruebaGit/pruebaGit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pruebaGit
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+           int master = 1;
+            int ra = 2;
+            int rt = 4;
+        }
+    }
+}
